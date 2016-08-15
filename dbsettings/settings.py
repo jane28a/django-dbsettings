861771b3897e209ce8ebb7fa5c7b80c65c093904
Andy Baker <andy@andybak.net>
2016-08-15 15:35:31 +0100

    larger max_length default

--- dbsettings/settings.py
+++ dbsettings/settings.py
@@ -5,4 +5,4 @@ from django.conf import settings
 sites_installed = apps.is_installed('django.contrib.sites')
 USE_SITES = getattr(settings, 'DBSETTINGS_USE_SITES', sites_installed)
 USE_CACHE = getattr(settings, 'DBSETTINGS_USE_CACHE', True)
-VALUE_LENGTH = getattr(settings, 'DBSETTINGS_VALUE_LENGTH', 255)
+VALUE_LENGTH = getattr(settings, 'DBSETTINGS_VALUE_LENGTH', 1024)  # It's my fork and I'll do what I want to...
